6700881e52aa58381b6d9bdf263b037e3fe27781
Hyyyp <781098538@qq.com>
2020-01-13 16:00:20 +0800

    index nav api finish

--- dist/js/common.js
+++ dist/js/common.js
@@ -44,4 +44,44 @@
 			index_list.eq(index).attr({ 'class': 'index_ac' });
 		}
 	});
+
+	$(function () {
+		$.ajax('http://localhost:3000/proDatas').then(function (data) {
+			console.log(data.magic);
+			var nav = $(".sp_all_ac");
+			var magic = data.magic;
+			var str = '';
+			var arr = [];
+
+			for (var i = 0; i < magic.length; i++) {
+				arr.push(magic[i].index * 1);
+			}
+
+			// 所有商品下拉菜单
+			for (var _i = 0; _i < data.result.length; _i++) {
+				if (arr.indexOf(_i) != -1) {
+					str += '<li class="' + data.result[_i][1] + '" data_i="' + data.result[_i][1] + '">\n\t\t\t\t\t\t<a href="#">' + data.result[_i][0] + '</a>\n\t\t\t\t\t\t<div class="sp_all_sec">';
+					for (var j = 0; j < magic[_i].pro.length; j++) {
+						str += '<div class="sp_all_thi"><h5 class="sp_title">' + magic[_i].pro[j].title + '</h5>';
+						for (var k = 0; k < magic[_i].pro[j].data.length; k++) {
+							if (magic[_i].pro[j].data[k].type) {
+								str += '<span class="sp_hot_r"><a>' + magic[_i].pro[j].data[k].t + '</a></span>';
+							} else {
+								str += '<span><a>' + magic[_i].pro[j].data[k].t + '</a></span>';
+							}
+						}
+						str += '</div>';
+					}
+					str += '</div></li>';
+				} else {
+					str += '<li class="' + data.result[_i][1] + '" data_i="' + data.result[_i][1] + '">\n\t\t\t\t\t<a href="#">' + data.result[_i][0] + '</a>\n\t\t\t\t\t</li>';
+				}
+			}
+			nav.html(str);
+
+			// 所有商品二级菜单
+			var nav_list = nav.children();
+			var str1 = '';
+		});
+	});
 })();
--- js/index.js
+++ js/index.js
@@ -43,4 +43,52 @@
 		}
 		
 	});
+	
+	$(function(){
+		$.ajax('http://localhost:3000/proDatas').then(function(data){
+			console.log(data.magic);
+			let nav = $(".sp_all_ac");
+			let magic = data.magic;
+			let str = ``;
+			let arr = [];
+			
+			for(let i = 0; i < magic.length; i++){
+				arr.push(magic[i].index * 1);
+			}
+			
+			// 所有商品下拉菜单
+			for(let i = 0; i < data.result.length; i++){
+				if(arr.indexOf(i) != -1){
+					str += `<li class="${data.result[i][1]}" data_i="${data.result[i][1]}">
+						<a href="#">${data.result[i][0]}</a>
+						<div class="sp_all_sec">`;
+					for(let j = 0; j < magic[i].pro.length; j++){
+						str += `<div class="sp_all_thi"><h5 class="sp_title">${magic[i].pro[j].title}</h5>`;
+						for(let k = 0; k < magic[i].pro[j].data.length; k++){
+							if(magic[i].pro[j].data[k].type){
+								str += `<span class="sp_hot_r"><a>${magic[i].pro[j].data[k].t}</a></span>`;
+							}else{
+								str += `<span><a>${magic[i].pro[j].data[k].t}</a></span>`;
+							}
+						}
+						str += `</div>`;
+					}
+					str += 	`</div></li>`;
+				}
+				
+				else{
+					str += `<li class="${data.result[i][1]}" data_i="${data.result[i][1]}">
+					<a href="#">${data.result[i][0]}</a>
+					</li>`
+				}
+			}
+			nav.html(str);
+			
+			// 所有商品二级菜单
+			let nav_list = nav.children();
+			let str1 = ``;
+			
+		});
+	});
+	
 })();
